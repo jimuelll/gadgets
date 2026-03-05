9aa076cfa5929f50bc82814b28d4ee10ef2ae006
Jimuel Untalan <131651496+jimuelll@users.noreply.github.com>
2026-03-05 11:20:30 +0800

    Add files via upload

---
+++ controllers/productController.js
@@ -0,0 +1,24 @@
+//SQL
+const connection = require("../config/db");
+//get all products
+exports.getAllGadgets = (req, res) => {
+  connection.query("SELECT * FROM products", (err, rows, fields) => {
+    if (err) throw err;
+    res.json(rows);
+  });
+};
+
+exports.createProducts = (req, res) => {
+  const { id, itemName, unitPrice, quantity, supplier } = req.body;
+  connection.query(
+    "INSERT INTO products (id, itemName, unitPrice, quantity, supplier) VALUES (?, ?, ?, ?, ?)",
+    [id, itemName, unitPrice, quantity, supplier],
+    (err, result) => {
+      if (err) throw err;
+      res.json({
+        message: "User created successfully",
+        id: result.insertId,
+      });
+    },
+  );
+};
